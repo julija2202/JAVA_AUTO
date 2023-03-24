43da4d90d63fa489dec550a440bd9c349e468235
julija2202 <julietkajagolovich@gmail.com>
2023-03-24 21:14:00 +0300

    commit4

---
+++ src/HW__5.java
@@ -0,0 +1,14 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class HW__5 {
+    public static void main(String[] args) {
+        for(int j =10; j>0; j--) {
+            for (int i = 0; i < j; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println( );
+        }
+
+
+    }
+}
