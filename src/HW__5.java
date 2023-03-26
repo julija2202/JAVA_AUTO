017f6f55a4c534e6640206833f7f0151443d3df2
julija2202 <julietkajagolovich@gmail.com>
2023-03-26 13:33:27 +0300

    commit6

--- src/HW__5.java
+++ src/HW__5.java
@@ -2,15 +2,15 @@ import com.sun.jdi.PathSearchingVirtualMachine;
 
 public class HW__5 {
     public static void main(String[] args) {
-        for(int j =10; j>0; j--) {
+        for (int j = 10; j > 0; j--) {
             for (int i = 0; i < j; i++) {
                 System.out.print(i + " ");
             }
-            System.out.println( );
+            System.out.println();
         }
 
-        for(int j =10; j>0; j--) {
-            for(int i =10;i>=j; i--){
+        for (int j = 10; j > 0; j--) {
+            for (int i = 10; i >= j; i--) {
                 System.out.print("   ");
             }
             for (int i = 0; i < j; i++) {
@@ -19,5 +19,19 @@ public class HW__5 {
             System.out.println(" ");
         }
 
+       for (int j = 10; j >= 0; j--) {
+            for (int i = 10; i > j; i--) {
+                System.out.print("   ");
+            }
+          for (int i = j-1; i >= 0; i--) {
+                System.out.print(i + " ");
+            }
+            for(int i = 1; i<j; i++){
+             System.out.print(i + " ");
+            }
+            System.out.println("   ");
+        }
+
+        }
     }
-}
+
